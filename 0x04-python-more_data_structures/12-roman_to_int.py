94e3f4823f6eb4b517db98a40a3febf54301d29f
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-05-06 21:09:18 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if (roman_string is None or type(roman_string) is not str):
         return 0
-    nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    for i in roman_string:
-        if i not in nums:
-            return 0
-    prev = nums.get(roman_string[0])
-    for j in roman_string:
-        num = nums.get(j)
-        if prev >= num:
-            result += num
-        elif prev < num:
-            result = result + num - prev - prev
-        prev = num
-    return result
