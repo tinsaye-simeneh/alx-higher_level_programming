8dd483a53bea5674565b6120e7104b9197cb4190
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-05-18 21:05:39 +0300

    Delete 0x06-python-classes directory

--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-class Square:
-    __size = 0
-
-    def __init__(self, new_size=0):
-        """Initialize class."""
-        if new_size is not 0:
-            self.__size = new_size
