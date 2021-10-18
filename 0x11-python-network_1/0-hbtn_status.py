fa9ca4827ba96beed49bef25095628b3912d8852
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2021-10-18 20:15:54 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
-if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
+'''script that fetches https://intranet.hbtn.io/status '''
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
         print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print('\t- type:', type(r))
+        print('\t- content:', r)
+        print('\t- utf8 content:', r.decode('utf-8'))
