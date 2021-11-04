d3b0d362670748c6b1b2322f596e42f9066d16d2
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2021-11-04 14:19:18 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const map1 = list.map((x, i) => i * x);
-console.log(list);
-console.log(map1);
+const array = require('./100-data').list;
+console.log(array);
+console.log(array.map(function (x, i) { return (x * i); }));
