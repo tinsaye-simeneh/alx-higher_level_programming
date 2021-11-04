13b0d9b1574705e63033609b16a0ddb17af885e5
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2021-11-04 14:16:40 +0300

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,8 @@
+
 #!/usr/bin/node
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+
+const fA = fs.readFileSync(process.argv[2], 'utf8');
+const fB = fs.readFileSync(process.argv[3], 'utf8');
+const answer = fA + fB;
+fs.writeFile(process.argv[4], answer, function (err) { if (err) throw err; });
