64a53593142996222446f6945e84fea4c9aacbc4
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2022-03-03 14:30:47 -0500

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 number = 98
-<<<<<<< HEAD
 print("{:d} Battery street".format(98))
-
-=======
-print("{} Battery street".format(98))
->>>>>>> 7ece05105249c1a77cd6f783fcda5ff30f21c794
